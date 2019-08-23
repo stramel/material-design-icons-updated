2c081e19fda0a9f19c1512de4858a60c62a3467b
Michael Stramel <m.stramel89@gmail.com>
2019-08-22 22:56:01 -0500

    Update to new format
    WIP 2
    
    WIP 3
    
    WIP 4
    
    WIP 5
    
    WIP 6
    
    WIP 7
    
    WIP 8
    
    WIP 9
    
    fixed
    
    WIP 10
    
    Finish
    
    Finish
    
    clean up
    
    update

--- scripts/download.js
+++ scripts/download.js
@@ -1,9 +1,15 @@
 const got = require('got')
 import { resolve, join } from 'path'
+import queue from 'async.queue'
+import progress from 'cli-progress'
 import { ensureDirectory, readFile, writeFile } from './fs.js'
 
 // Pull in our current version
 const versionFilePath = resolve(__dirname, '../VERSION')
+
+const bar = new progress.SingleBar({}, progress.Presets.shades_classic)
+
+const errorMap = new Map()
 
 /**
  *
@@ -14,12 +20,13 @@ async function hasUpdate(version) {
   const { body: data } = await got(
     'https://fonts.googleapis.com/icon?family=Material+Icons',
   )
-  const { version: googleVersion } = data.match(
-    /https:\/\/fonts\.gstatic\.com\/s\/materialicons\/v(?<version>[\d]+)/,
+  const { version: googleVersion, hash } = data.match(
+    /https:\/\/fonts\.gstatic\.com\/s\/materialicons\/v(?<version>[\d]+)\/(?<hash>[A-Za-z0-9\-]+)\./,
   ).groups
 
-  if (parseInt(googleVersion) > parseInt(version)) {
-    return googleVersion
+  const hashedVersion = `${googleVersion}-${hash}`
+  if (hashedVersion !== version) {
+    return hashedVersion
   }
 
   return false
@@ -72,17 +79,9 @@ const themes = ['baseline', 'outline', 'round', 'sharp', 'twotone']
  * @param {Theme} theme
  */
 function buildIconUrl(icon, theme) {
-  /**
-   * @type {string}
-   */
-  let fileName
-  if (icon.imageUrls && icon.imageUrls[theme]) {
-    fileName = icon.imageUrls[theme]
-  } else {
-    fileName = `${theme}-${icon.id}-24px.svg`
-  }
-
-  return `https://material.io/tools/icons/static/icons/${fileName}`
+  return `https://fonts.gstatic.com/s/i/materialicons${theme}/${
+    icon.id
+  }/v1/24px.svg?download=true`
 }
 
 /**
@@ -103,15 +102,17 @@ const themeNameMap = {
  * @param {Theme} theme
  */
 async function downloadAndSave(category, icon, theme) {
-  const url = buildIconUrl(icon, theme)
-  const size = url.match(/^.*-([0-9]+)px.svg$/)[1]
+  const url = buildIconUrl(
+    icon,
+    theme === 'baseline' ? '' : theme === 'outline' ? 'outlined' : theme,
+  )
   const { body: svg } = await got(url)
   const dir = join(
     __dirname,
     `../icons/${themeNameMap[theme]}/${category.name}`,
   )
   await ensureDirectory(dir)
-  await writeFile(join(dir, `ic_${icon.id}_${size}px.svg`), svg)
+  await writeFile(join(dir, `ic_${icon.id}_24px.svg`), svg)
 }
 
 async function run() {
@@ -123,26 +124,63 @@ async function run() {
       console.log('No update found')
       return
     }
-    console.log('New version found: ', updatedVersion)
+    console.log(`New version found: v${updatedVersion.split('-')[0]}`)
 
     const manifest = await getManifest()
-    // manifest.categories = manifest.categories.splice(1, 1)
+
+    const q = queue(async ({ task: iconCombo }, callback) => {
+      try {
+        await downloadAndSave(...iconCombo)
+      } catch (err) {
+        callback(true)
+        return
+      }
+      callback()
+    }, 5)
+
+    const p = new Promise(resolve => {
+      q.drain = resolve
+    })
+
+    const iconCominations = []
+
+    // Generating all icon combinations
+    manifest.categories.forEach(category => {
+      category.icons.forEach(icon => {
+        themes.forEach(theme => {
+          iconCominations.push({ task: [category, icon, theme] })
+        })
+      })
+    })
+
+    let total = iconCominations.length
+
+    console.log(`Downloading ${total} icons:`)
+    bar.start(total, 0)
 
     // Download all Icons
-    await Promise.all(
-      manifest.categories.map(category => {
-        return Promise.all(
-          category.icons.map(icon => {
-            return Promise.all(
-              themes.map(theme => {
-                console.log(`Downloading: ${theme} ${icon}`)
-                return downloadAndSave(category, icon, theme)
-              }),
-            )
-          }),
-        )
-      }),
-    )
+    iconCominations.forEach(task => {
+      q.push(task, err => {
+        if (err) {
+          const tries = errorMap.get(task) || 0
+          if (tries < 3) {
+            q.push(task)
+            errorMap.set(task, tries + 1)
+            total += 1
+            bar.setTotal(total)
+            return
+          }
+          console.error('Failed to download.')
+          exit(1)
+          return
+        }
+        bar.increment()
+      })
+    })
+
+    await p
+
+    bar.stop()
 
     await Promise.all([
       writeFile(
@@ -152,7 +190,7 @@ async function run() {
       writeFile(versionFilePath, updatedVersion),
     ])
 
-    console.log(`Successfully updated to v${updatedVersion}!`)
+    console.log(`Successfully updated to v${updatedVersion.split('-')[0]}!`)
   } catch (err) {
     console.error('UNEXPECTED ERROR:', err)
     throw err
